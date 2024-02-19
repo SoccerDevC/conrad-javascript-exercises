ddc0d43fdd3f7203253cff56ee20d96a2597c34f
SoccerDevC <cddavid2001@gmail.com>
2024-02-19 12:51:37 +0300

    all tests have been passed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let newArray = word.split("");
+    const reverseWord = [];
+    let j=0;
+    for(let i = newArray.length -1 ; i>=0 ; i--){
+        reverseWord[j] = newArray[i];
+        j++;
+    }
+    let newWord = reverseWord.join("");
+    return newWord;
 };
 
 // Do not edit below this line
