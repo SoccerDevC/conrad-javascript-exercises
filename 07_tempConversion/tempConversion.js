068babe7819be449b09f9b6519c2c381ffba066d
SoccerDevC <cddavid2001@gmail.com>
2024-02-20 00:37:33 +0300

    All tests have been passeds

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let newTemp = (temp - 32) * .5556;
+  let roundedTemp = Math.round(newTemp * 10)/10;
+  return roundedTemp;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let newTemp = (temp * 1.8) + 32;
+  let roundedTemp = Math.round(newTemp * 10)/10;
+  return roundedTemp;
 };
 
 // Do not edit below this line
