e2041abcd4bad8cca8f0744459f722814412ad05
SoccerDevC <cddavid2001@gmail.com>
2024-02-19 13:24:02 +0300

    All tests are working as expected

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,14 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...valuesToRemove) {
+    const newArray = [];
+    for (let i = 0; i < array.length; i++) {
+        if (valuesToRemove.includes(array[i])) {
+            continue; // Skip if the value matches any of the values to be removed
+        }
+        newArray.push(array[i]); // Add all other values to the new array
+    }
+    return newArray;
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
