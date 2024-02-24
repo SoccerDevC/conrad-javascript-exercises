22d53923ac6a6a2be5fb5aaffafe9c4a07f4a853
SoccerDevC <cddavid2001@gmail.com>
2024-02-24 10:09:37 +0300

    the calculator with all its functions working

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+	return numbers.reduce((total,currentItem) => total + currentItem,0 );
 };
 
-const multiply = function() {
-
+const multiply = function(nums2) {
+  return nums2.reduce((total, currentItem)=> total*currentItem, 1);
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+  const powerArray = [];
+  for(let i =0; i<num2; i++){
+    powerArray[i]=num1;
+  }
+	return powerArray.reduce((total,currentItem) => total*currentItem, 1)
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if(num === 0){
+    return 1;
+  }else{
+
+    const array = [];
+    let curr = num;
+
+    for(let i = 0; i<num; i++){
+      array[i]=curr;
+      curr--;
+    }
+    return array.reduce((total, currentItem)=> total*currentItem, 1);
+  }
 };
 
 // Do not edit below this line
