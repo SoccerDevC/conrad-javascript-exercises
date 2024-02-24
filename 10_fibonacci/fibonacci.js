7f1894e9b2bbd76b8111d0a89229c3c045cf7331
SoccerDevC <cddavid2001@gmail.com>
2024-02-24 10:41:11 +0300

    fully completed assignment with all tests working

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,23 @@
-const fibonacci = function() {
-
+const fibonacci = function(value) {
+    let num = parseInt(value);
+    if(num ===0){
+        return 0;
+    }
+    else if(num < 0){
+        return "OOPS";
+    }
+    else{
+        const fibonnaciSequence = [];
+        for(let i=0; i<num; i++){
+            if(i===0 || i ===1){
+                fibonnaciSequence[i] = 1;
+            }
+            else{
+                fibonnaciSequence[i] = fibonnaciSequence[i-1] + fibonnaciSequence[i-2];
+            }
+        }
+        return fibonnaciSequence[fibonnaciSequence.length-1];
+    }
 };
 
 // Do not edit below this line
