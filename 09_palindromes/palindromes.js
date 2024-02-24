80dffa0f28cc4306e504103a4f5f01092a73ec50
SoccerDevC <cddavid2001@gmail.com>
2024-02-24 10:27:57 +0300

    fully working palindromes function

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (statement) {
+    
+   let newStatement = statement.split(" ").join("");
+    newStatement = statement.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
+   let reverseStatement = newStatement.split("").reverse().join("");
+   return newStatement ===reverseStatement;
 };
 
 // Do not edit below this line
